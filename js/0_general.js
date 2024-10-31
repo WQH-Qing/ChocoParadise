1404507d7ae245103ff6bb0c02d3ae183a6d8875
WQH-Qing <berrytea0605@gmail.com>
2024-10-31 19:06:04 +0800

    add

--- js/0_general.js
+++ js/0_general.js
@@ -1,3 +1,32 @@
-$('.top').click(function() {
-    $('window').scrollTop(0)
+$(document).ready(function(){
+
+    // let scroll_Top = $(window).scrollTop()
+    // if (scroll_Top <= 0){
+    //     $('.top').hide()
+    // }else{
+    //     $('.top').show()
+    // }
+
+    // let scroll_Top = $(window).scrollTop()
+    // if (scroll_Top == 0){
+    //     $('.top').addclass('hide')
+    // }else{
+    //     $('.top').removeclass('hide')
+    // }    
+ 
+   
+
+        $('.top').click(function() {
+        $(window).scrollTop(0)
+    })
+
+
+
+    $('.hamburger').click(function() {
+        $('.hamburger').toggleClass('active') 
+        $('.list').toggleClass('active')
+    })
+
+
+
 })
